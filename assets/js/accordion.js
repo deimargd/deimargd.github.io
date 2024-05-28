634bb38ec8353a9787155190575ee46e096c4715
deimargd <139195070+deimargd@users.noreply.github.com>
2024-05-28 15:54:01 -0500

    update accordion

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,22 +1,31 @@
-  document.addEventListener("DOMContentLoaded", function() {
-    var faqSection = document.querySelector('.faq');
-    var faqWrapper = document.querySelector('.faq-wrapper');
-    var faqToggle = document.createElement('i');
-    faqToggle.classList.add('faq-wrapper-toggle', 'bi', 'bi-chevron-down');
-    
-    document.querySelector('.faq-section-title').appendChild(faqToggle);
-    
-    document.querySelector('.faq-section-title').addEventListener('click', function() {
-      faqWrapper.classList.toggle('faq-wrapper-active');
-      faqToggle.classList.toggle('faq-wrapper-toggle-active');
-    });
 
-    // Handle individual FAQ accordion functionality
-    var faqItems = document.querySelectorAll('.faq-item');
-    faqItems.forEach(function(item) {
-      item.addEventListener('click', function() {
-        item.classList.toggle('faq-active');
-      });
+document.addEventListener("DOMContentLoaded", function() {
+  // Select required elements
+  var faqSection = document.querySelector('.faq');
+  var faqWrapper = document.querySelector('.faq-wrapper');
+  var faqToggle = document.querySelector('.faq-wrapper-toggle');
+  var faqTitle = document.querySelector('.faq-section-title');
+
+  // Toggle function for entire FAQ section
+  faqTitle.addEventListener('click', function() {
+    faqWrapper.classList.toggle('faq-wrapper-active');
+    faqToggle.classList.toggle('faq-wrapper-toggle-active');
+  });
+
+  // Handle individual FAQ item toggles
+  var faqItems = document.querySelectorAll('.faq-item h3');
+  faqItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      var parent = item.parentNode;
+      parent.classList.toggle('faq-active');
+      
+      var content = parent.querySelector('.faq-content');
+      if (parent.classList.contains('faq-active')) {
+        content.style.display = 'block';
+      } else {
+        content.style.display = 'none';
+      }
     });
   });
+});
 
