a0d2829ec9e609ebbd50cd4b2e8fa6f295af8b1b
deimargd <139195070+deimargd@users.noreply.github.com>
2024-06-13 20:03:29 -0500

    updates css faqs

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,14 +171,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   window.addEventListener('load', initIsotopeLayout);
 
-  /**
-   * Frequently Asked Questions Toggle
-   */
-  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
-    faqItem.addEventListener('click', () => {
+/**
+ * Frequently Asked Questions Toggle
+ */
+document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+  faqItem.addEventListener('click', () => {
       faqItem.parentNode.classList.toggle('faq-active');
-    });
   });
+});
+
 
   /**
    * Init swiper sliders
